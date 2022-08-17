664e40e0648b1781109b0d50549ce7688eb884aa
Youyi Fong <youyifong@gmail.com>
2022-08-17 13:46:08 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+from setuptools import setup
+
+install_deps = ['numpy>=1.20.0', 
+                'os', 
+                'matplotlib.pyplot',
+                'glob', 
+                'read_roi', 
+                'PIL'
+                ]
+
+    
+setup(
+    name="syotil",
+    license="BSD",
+    author="Sunwoo Han and Youyi Fong",
+    author_email="sunwooya@gmail.com",
+    description="utility functions",
+    url="https://github.com/youyifong/syotil",
+    packages=setuptools.find_packages(),
+    use_scm_version=True,
+    install_requires = install_deps,
+    include_package_data=True,
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ),
+)
