5243328da252b45f3bf9758e16cc1da3b5026727
Youyi Fong <youyifong@gmail.com>
2022-11-07 18:00:57 -0800

    Update __main__.py

--- syotil/__main__.py
+++ syotil/__main__.py
@@ -32,8 +32,8 @@ def main():
         if file_extension1==".png":
             mask1=imread(args.mask1)
         elif file_extension1==".npz":
-            masks1 = np.load(args.mask1, allow_pickle=True)
-            masks1 = mask1['masks']
+            mask1 = np.load(args.mask1, allow_pickle=True)
+            mask1 = mask1['masks']
         else:
             print("file type not supported: "+file_extension1)
             
@@ -41,8 +41,8 @@ def main():
         if file_extension2==".png":
             mask2=imread(args.mask2)
         elif file_extension2==".npz":
-            masks2 = np.load(args.mask2, allow_pickle=True)
-            masks2 = mask2['masks']
+            mask2 = np.load(args.mask2, allow_pickle=True)
+            mask2 = mask2['masks']
             
         out=csi(mask1, mask2)
         print(out)
