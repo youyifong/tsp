623fae7e1e40d1381bbbd29e177d22fbf44592c0
Youyi Fong <youyifong@gmail.com>
2023-03-10 13:56:26 -0800

    motion

--- syotil/__main__.py
+++ syotil/__main__.py
@@ -97,44 +97,40 @@ def main():
         
 
     elif args.action=="alignimages":
+        
         filename, file_extension = os.path.splitext(args.image2)
+
         image1=imread(args.ref_image)
         image2=imread(args.image2)    
-        ## check the frequency of 0 in intensity distribution
-        # print(np.histogram(image2, bins=np.append(np.unique(image2), np.inf)))
+
+        sz = image1.shape
     
         # Convert images to grayscale for computing the rotation via ECC method
-        im1_gray = cv2.cvtColor(image1,cv2.COLOR_BGR2GRAY)
-        im2_gray = cv2.cvtColor(image2,cv2.COLOR_BGR2GRAY)
-         
-        # Find size of image1
-        sz = image1.shape
-         
-        # Define the motion model - euclidean is rigid (SRT)
-        warp_mode = cv2.MOTION_HOMOGRAPHY 
-        warp_matrix = np.eye(3, 3, dtype=np.float32)
+        if len(sz)==3:
+            im1_gray = cv2.cvtColor(image1,cv2.COLOR_BGR2GRAY)
+            im2_gray = cv2.cvtColor(image2,cv2.COLOR_BGR2GRAY)         
+        elif len(sz)==2:
+            im1_gray = image1
+            im2_gray = image2
         
-        # #MOTION_AFFINE does not work well for some reason
-        # warp_mode = cv2.MOTION_AFFINE 
-        # warp_matrix = np.eye(2, 3, dtype=np.float32)
-
-        # Specify the number of iterations.
-        number_of_iterations = 15000;
-         
-        # Specify the threshold of the increment
-        # in the correlation coefficient between two iterations
-        termination_eps = 1e-2;
-         
-        # Define termination criteria
+        # motion models: MOTION_HOMOGRAPHY, MOTION_AFFINE, MOTION_EUCLIDEAN (rigid), MOTION_TRANSLATION 
+        warp_mode = cv2.MOTION_HOMOGRAPHY  
+        warp_matrix = np.eye(3 if warp_mode == cv2.MOTION_HOMOGRAPHY else 2, 3, dtype=np.float32)
+        
+        number_of_iterations = 15000;         
+        termination_eps = 1e-2; # Specify the threshold of the increment in the correlation coefficient between two iterations
         criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, number_of_iterations,  termination_eps)
          
         # Run the ECC algorithm. The results are stored in warp_matrix.
         (cc, warp_matrix) = cv2.findTransformECC (im1_gray, im2_gray, warp_matrix, warp_mode, criteria, None, 1)
                  
-        # Warp im2 
-        # warp_matrix, _ =cv2.estimateAffine2D (im1_gray, im2_gray)
-        image2_warp = cv2.warpPerspective (image2, warp_matrix, (sz[1],sz[0]), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP);
+        if warp_mode == cv2.MOTION_HOMOGRAPHY :
+            image2_warp = cv2.warpPerspective (image2, warp_matrix, (sz[1],sz[0]), flags=cv2.INTER_NEAREST + cv2.WARP_INVERSE_MAP); # INTER_LINEAR or INTER_NEAREST
+        else :        
+            image2_warp = cv2.warpAffine (image2, warp_matrix, (sz[1],sz[0]), flags=cv2.INTER_NEAREST + cv2.WARP_INVERSE_MAP);
         
+        imsave(filename+"_aligned"+file_extension,  image2_warp)
+
          
         ## elastix outcomes are blurred. In histograms, the background value 0 is not a peak
         # # Convert the images to gray level: color is not supported. This step can be changed to average across channels
@@ -162,10 +158,9 @@ def main():
         # print(np.histogram(image2_warp, bins=np.append(np.unique(image2_warp), np.inf)))
                 
         # save to file
-        # print(image2_warp.shape) %2D image
-        image2_warp=image_to_rgb(image2_warp) # now 3D
+        # print(image2_warp.shape) %3D image after cv2
+        # image2_warp=image_to_rgb(image2_warp) # now 3D
         # print(np.histogram(image2_warp, bins=np.append(np.unique(image2_warp), np.inf)))
-        imsave(filename+"_aligned"+file_extension,  image2_warp)
 
         
     elif args.action=='overlaymasks':
