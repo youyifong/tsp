d590a070ed94265210afd0cffa16d4955daf4db9
Youyi Fong <youyifong@gmail.com>
2023-03-30 14:46:40 -0700

    Update intensityanalysis.py

--- tsp/intensityanalysis.py
+++ tsp/intensityanalysis.py
@@ -10,15 +10,14 @@ def IntensityAnalysis(mask_file, image_files, channel=None):
     dat = np.load(mask_file, allow_pickle=True).item()
     masks = dat['masks']
     
-    filenames=[os.path.splitext(f)[0] for f in image_files]
-
     centers = GetCenterCoor(masks)
     y_coor=[i[0] for i in centers]
     x_coor=[i[1] for i in centers]
 
     mask_indices = np.unique(masks)[1:]
+    cell_name = ["Cell_"+str(i) for i in mask_indices]
 
-    res = [["Cell_"+str(i) for i in mask_indices], x_coor, y_coor]
+    res = [cell_name, x_coor, y_coor]
 
     for i in range(len(image_files)):
         im = imread(image_files[i])
@@ -26,7 +25,7 @@ def IntensityAnalysis(mask_file, image_files, channel=None):
         res.append(ndimage.mean(im, labels=masks, index=mask_indices))
         
     res = pd.DataFrame(res).T
-    res.columns = ["name", "x","y"] +filenames
+    res.columns = ["name", "x","y"] + [os.path.splitext(f)[0] for f in image_files]
     res.to_csv(os.path.splitext(mask_file)[0] + "_MFI.csv", header=True, index=False, sep=',')
 
 
