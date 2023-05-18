a4075ece013dfc0bc1fc7761eeb6193d226d2af6
Youyi Fong <youyifong@gmail.com>
2023-05-18 11:46:19 -0700

    updates

--- tsp/__main__.py
+++ tsp/__main__.py
@@ -8,7 +8,7 @@ from tsp.runcellpose import run_cellpose
 from tsp.cellphenotyping import StainingAnalysis
 from tsp.intensityanalysis import IntensityAnalysis
 from tsp.geom import dist2boundary, region_membership
-from tsp.split_dataset import split_dataset
+from tsp.split_dataset import split_dataset_by_class
 
 import timeit
 start_time = timeit.default_timer()
@@ -77,6 +77,8 @@ def main():
     
     # splitdataset
     parser.add_argument('--dataset', type=str, help='dataset folder', required=False)
+    parser.add_argument('--imgext', type=str, help='image file extension, e.g. jpg, png, tif', required=False)
+    parser.add_argument('--trainratio', type=str, help='train to all, e.g. 0.9', required=False)
     
     args = parser.parse_args()
 
@@ -386,7 +388,9 @@ def main():
             print(" \\\\\n".join([",".join(map(str,line)) for line in res_temp])) # csv format
         
     elif args.action=='splitdataset':
-        split_dataset(args.dataset)
+        imgext=args.imgext if args.imgext is not None else 'tif'
+        trainratio=float(args.trainratio if args.trainratio is not None else '0.9')
+        split_dataset_by_class(args.dataset, imgext, trainratio)
         
 
     print(f"time passed: {(timeit.default_timer() - start_time)/60:.1f} min"); 
--- tsp/split_dataset.py
+++ tsp/split_dataset.py
@@ -3,14 +3,16 @@ import os
 from PIL import Image
 
 
-def split_dataset(DATASET_FOLDER_NAME, TRAIN_RATIO = 0.9):
+def split_dataset_by_class(DATASET_FOLDER_NAME, IMG_EXT='tif', TRAIN_RATIO = 0.9):
     class_names = os.listdir(DATASET_FOLDER_NAME)
     test_path, train_path = prepare_split_target_folders()
+    print(f"IMG_EXT: {IMG_EXT}, TRAIN_RATIO: {TRAIN_RATIO}")
 
     for class_name in class_names:
         current_class_path = os.path.join(DATASET_FOLDER_NAME, class_name)
-        current_class_images = list((filter(lambda filename: filename.endswith('tif'), os.listdir(current_class_path))))
+        current_class_images = list((filter(lambda filename: filename.endswith(IMG_EXT), os.listdir(current_class_path))))
         number_of_images_in_class = len(current_class_images)
+        print(f"{class_name} number_of_images_in_class: {number_of_images_in_class}")
 
         sampling_permuatation = numpy.random.permutation(number_of_images_in_class)
         train_index_bound = int(TRAIN_RATIO * number_of_images_in_class)
