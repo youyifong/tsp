17510f0d8477c781fe3e7de13f9db8d820de68ed
Youyi Fong <youyifong@gmail.com>
2023-07-03 16:54:43 -0700

    Update __main__.py

--- tsp/__main__.py
+++ tsp/__main__.py
@@ -2,8 +2,8 @@ import argparse, glob, os, sys
 import numpy as np
 
 from tsp import imread, imsave, image_to_rgb, normalize99
-from tsp.masks import mask2outline, roifiles2mask
-from tsp.AP import masks_to_outlines, tp_fp_fn, tpfpfn, csi, bias, color_fp_fn, compute_iou
+from tsp.masks import roifiles2mask
+from tsp.AP import mask2outline, masks_to_outlines, tp_fp_fn, tpfpfn, csi, bias, color_fp_fn, compute_iou
 from tsp.alignment import doalign
 from tsp.runcellpose import run_cellpose
 from tsp.cellphenotyping import StainingAnalysis
